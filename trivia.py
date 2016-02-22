2aa4c719a22429a0d2fcb1c6db4bb15f2a083277
isabellyfd <icfd@cin.ufpe.br>
2016-02-22 10:44:13 -0300

    Primeira aula

---
+++ trivia.py
@@ -0,0 +1,32 @@
+# 22/02/2016 lesson
+
+p1 = "Qual a capital da Croacia?"
+respostaP1 = "Zagreb"
+
+p2 = "Em que ano Python 1.0 tornou-se disponivel?"
+respostaP2 = "1994"
+
+p3 = "Qual o nome do segundo album do Vampire Weekend?"
+respostaP3 = "Contra"
+
+pergunta4 = "Qual o nome do 'pai' da computação?"
+resposta_pergunta4 = "Alan Turin"
+
+pergunta5 = "Qual a ultima horcrux a ser descoberta em Harry Potter?"
+resposta_pergunta5 = "Ele mesmo"
+
+#Mostrando as perguntas
+print( p1 )
+tentativa1 = input()
+
+print( p2 )
+tentativa2 = input()
+
+print( p3 )
+tentativa3 = input()
+
+print( pergunta4 )
+tentativa4 = input()
+
+print( pergunta5 )
+tentativa5 = input()
