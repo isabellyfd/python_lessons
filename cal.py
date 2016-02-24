c55c9b82983cb17673cdd2a28a53f281152f657e
unknown <user-bepid@g3c23.windows.cin.ufpe.br>
2016-02-24 10:59:40 -0300

    Lesson 24/02/2016

--- cal.py
+++ cal.py
@@ -1,20 +1,62 @@
-operador = input("digite o operador:\n")
 
-if operador == "+":
-    number1 = int(input("num1:\n"))
-    number2 = int(input("num2:\n"))
-    print (number1 + number2)
-elif operador == "-":
-    number1 = int(input("num1:\n"))
-    number2 = int(input("num2:\n"))
-    print (number1 - number2)
-elif operador == "*":
-    number1 = int(input("num1:\n"))
-    number2 = int(input("num2:\n"))
-    print (number1 * number2)
-elif operador == "/":
-    number1 = int(input("num1:\n"))
-    number2 = int(input("num2:\n"))
-    print (number1 / number2)
-else:
-    print ("Oloooooco")
+count = 0
+memoria = 0
+M = False
+while count < 3:
+    operador = input("digite o operador (+, -, *, /, M ) :\n")
+    if operador == "+":
+        if M:
+            string = input("num1 ou m:\n")
+            if string == "m" or string == "M":
+                number1 = memoria
+            else:
+                number1 = int(string)
+        else:
+            number1 = int(input("num1:\n"))
+        number2 = int(input("num2:\n"))
+        memoria = number1 + number2
+        print (memoria)
+    elif operador == "-":
+        if M:
+            string = input("num1 ou m:\n")
+            if string == "m" or string == "M":
+                number1 = memoria
+            else:
+                number1 = int(string)
+        else:
+            number1 = int(input("num1:\n"))
+        number2 = int(input("num2:\n"))
+        memoria = number1 - number2
+        print (memoria)
+    elif operador == "*":
+        if M:
+            string = input("num1 ou m:\n")
+            if string == "m" or string == "M":
+                number1 = memoria
+            else:
+                number1 = int(string)
+        else:
+            number1 = int(input("num1:\n"))
+        number2 = int(input("num2:\n"))
+        memoria = number1 * number2
+        print (memoria)
+    elif operador == "/":
+        if M:
+            string = input("num1 ou m:\n")
+            if string == "m" or string == "M":
+                number1 = memoria
+            else:
+                number1 = int(string)
+        else:
+            number1 = int(input("num1:\n"))
+        number2 = int(input("num2:\n"))
+        if number2 != 0:
+            memoria = number1 / number2
+            print (memoria)
+        else:
+            print ("Não pode dividir por zero, honey!")
+    elif operador == "M":
+        M = True
+    else:
+        print ("Oloooooco, cê botou algo errado!")
+    count += 1
--- player1-2.py
+++ player1-2.py
@@ -5,76 +5,62 @@ print ("Hello Players, Lat's play de dumb game!!!")
 print ("Palyer 1: Please put 2 numbers!!")
 number1 = int(input("Number 1:"))
 number2 = int(input("Number 2:"))
-
+ 
 spaces = "\n"* 100
-
-isMajor = "Ruge than"
-isMinor = "Small than"
+ 
+isMajor = "Ruge than the major number"
+isMinor = "Small than the minor number"
 middleman = "its in the middle, man"
 countDown = 3
 flag1 = False
 flag2 = False
 print(spaces)
-print("Now, Player 2, you have", countDown, "guesses, guess a number")
-guess = int(input())
 
-if guess == number1:
-    flag1 = True
-elif guess == number2:
-    flag2 = True
-elif (guess > number1 and guess < number2) or (guess > number2 and guess < number1):
-    print(middleman)
-elif guess < number1:
-    print(isMinor, "number 1")
-elif guess < number2:
-    print (isMinor, "number 2")
-elif guess > number1:
-    print(isMajor, "number 1")
-elif guess > number2:
-    print (isMajor, "number 2")
-countDown -= 1
-
-
-done = flag1 and flag2
-if not done:
-    print ("Now, you have", countDown, "guesses, guess a number")
-    guess = int(input())
-    if guess == number1:
-        flag1 = True
-    elif guess == number2:
-        flag2 = True
-    elif (guess > number1 and guess < number2) or (guess > number2 and guess < number1):
-        print(middleman)
-    elif guess < number1:
-        print(isMinor, "number 1")
-    elif guess < number2:
-        print (isMinor, "number 2")
-    elif guess > number1:
-        print(isMajor, "number 1")
-    elif guess > number2:
-        print (isMajor, "number 2")
-    countDown -= 1
+maior = 0
+menor = 0
+if number1 > number2:
+    maior = number1
+    menor = number2
+else:
+    maior = number2
+    menor = number1
+    
+minrange = 10
+maxrange = 25
+while countDown > 0:
     done = flag1 and flag2
     if not done:
-        print ("Now you have", countDown, "guess, last chance to win, guess a fucking number!!!!")
+        print("Now, Player 2, you have", countDown, "guesses, guess a number")
         guess = int(input())
-        if guess == number1:
-            flag1 = True
-        elif guess == number2:
-            flag2 = True
-        elif (guess > number1 and guess < number2) or (guess > number2 and guess < number1):
-            print(middleman)
-        elif guess < number1:
-            print(isMinor, "number 1")
-        elif guess < number2:
-            print (isMinor, "number 2")
-        elif guess > number1:
-            print(isMajor, "number 1")
-        elif guess > number2:
-            print (isMajor, "number 2")
-        countDown -= 1
 
+        if guess >= 0 and guess <= 100:
+            if (guess < (menor-maxrange)):
+                countDown = 0
+            elif (guess > (maior+maxrange)):
+                countDown = 0
+            elif (guess < (menor-minrange)):
+                print ("maaaan c'mon, u noooooooby")
+            elif (guess > (maior+minrange)):
+                print ("maaaan c'mon, u noooooooby")
+            else:
+                if guess < menor:
+                    print (isMinor)
+                elif guess > maior:
+                    print (isMajor)
+                elif menor < guess < maior:
+                    print (middleman)
+                elif guess == menor:
+                    flag1 = True
+                elif guess == maior:
+                    flag2 = True
+                elif guess == menor and guess == maior:
+                    flag1 = True
+                    flag2 = True   
+            countDown -= 1
+    else:
+        countDown =0
+        
 if not done:
     print("man u lost!")
 else:
-    print("Congrats u won!")
+    print("You won!!!")
