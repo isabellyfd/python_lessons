0a0e78289cbf47241d9091209e6269e214c208d0
Isabelly Cristina Ferreira Damascena <icfd@ug1c14.windows.cin.ufpe.br>
2016-03-03 12:04:53 -0300

    haming man interface, begin

--- hangingman.py
+++ hangingman.py
@@ -3,6 +3,18 @@
 from tkinter import *
 
 root = Tk()
-w = Label(root, text="Hello Tkinter!")
-#w.pack()
+topFrame = Frame(root, bg="white")
+topFrame.pack()
+nameGame = Label(topFrame, text="Haging Man", bg="white")
+nameGame.pack()
+
+palavraLabel =  Label(topFrame, text="Digite aqui a palavra do jogo:", bg="white")
+palavraLabel.pack()
+
+submitPalavra  = Button(topFrame, text="Submit", fg="white", bg="black")
+submitPalavra.pack(side=RIGHT)
+
+bottomFrame = Frame(root, bg ="white")
+bottomFrame.pack(side=BOTTOM)
 root.mainloop()
+
